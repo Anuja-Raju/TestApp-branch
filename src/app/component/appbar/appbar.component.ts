778c0f23a608d3459b56de22d7e108a54d154969
Anuja <anujaraju23@gmail.com>
2024-02-26 11:10:45 +0530

    3rd commit

--- src/app/component/appbar/appbar.component.ts
+++ src/app/component/appbar/appbar.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-appbar',
   templateUrl: './appbar.component.html',
-  styleUrl: './appbar.component.css'
+  styleUrls: ['./appbar.component.css'] // Corrected property name to styleUrls
 })
 export class AppbarComponent {
 
-   
   constructor(private router: Router) {}
-
   
+  logout() {
+    this.router.navigate(['/login']);
+  }
+
 }
